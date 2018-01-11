003854b5a3ba3612ad4c3390e24982fb2f151e73
priyava <30829489+priyava@users.noreply.github.com>
2018-01-11 16:16:34 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class factorial
+	public static void main(String[]args)
+	{
+		int n=5;
+		int i,fact=1;
+		for(i=1;i<=5;i++)
+		{
+		fact=fact*i;
+		}
+		System.out.Println("\n tha fatorial is"+fact);
+	}
+}
