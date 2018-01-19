b1c7731dfeb89d0bf6cb81d0e2ce9e70cb95dd40
priyava <30829489+priyava@users.noreply.github.com>
2018-01-19 16:30:23 +0530

    Create hour.java

---
+++ hour.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class hour
+{
+public static void main(String args[])
+{
+int t,hours,minutes;
+System.out.println("\n Enter the time in minutes"+t);
+hours=t/60;
+minutes=t%60;
+System.out.println("\n %d %d"+hours,+minutes);
+}
+}
