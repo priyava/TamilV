ff95255276d071eb621676297471aed5ce4aea44
priyava <30829489+priyava@users.noreply.github.com>
2018-01-21 12:10:48 +0530

    Create primenum.java

---
+++ primenum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class primenum
+{
+public static void main(String ags[])
+{
+int l,r,i,flag;
+System.out.println("Enter the first range"+l);
+System.out.println("\n Enter the second range"+r);
+while(l<m)
+{
+flag=0;
+for(i=1;i<=n/2;i++)
+{
+if(l%i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+System.out.println(" "+l);
+l++;
+}
+}
+}
