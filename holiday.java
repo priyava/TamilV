854a020fcfd75887fe5bc5c2856c1b359f880bea
priyava <30829489+priyava@users.noreply.github.com>
2018-01-21 10:07:47 +0530

    Create holiday.java

---
+++ holiday.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class holiday
+{
+public static void main(String args[])
+{
+String str;
+System.out.println("\n Enter the day"+str);
+switch(str)
+{
+case "monday":
+System.out.println("yes");
+break;
+case "tuesday":
+System.out.println("yes");
+break;
+case "wednesday":
+System.out.println("yes");
+break;
+case "thursday":
+System.out.println("yes");
+break;
+case "friday":
+System.out.println("yes");
+break;
+case "saturday":
+System.out.println("no");
+break;
+case "sunday":
+System.out.println("no");
+break;
+}
+}
+}
+
+
