1ad5d956272544ca86e479f2063fd73895cafbd2
priyava <30829489+priyava@users.noreply.github.com>
2018-01-23 16:34:40 +0530

    Create nextnum.java

---
+++ nextnum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class nextnumber
+{
+public static void main(String args[])
+{
+int num,nextnum;
+System.out.println("\n Enter the number"+num);
+nextnum=num+1;
+System.out.println("\n The next number is"+nextnum);
+}
+}
