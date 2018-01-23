2b5b3c23e693ce6feaca60b80df5ac4a1b64a89c
priyava <30829489+priyava@users.noreply.github.com>
2018-01-23 16:11:44 +0530

    Create laptop.java

---
+++ laptop.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class laptop
+{
+public static void main(String agr[])
+{
+int k,i;
+String s;
+System.out.println("Enter the name of the string"+s);
+System.out.println("Enter the number to repeat the string"+k);
+for(i=0;i<k;i++)
+{
+System.out.println(" "+s);
+}
+}
+}
