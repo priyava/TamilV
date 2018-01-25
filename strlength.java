32a2f5bfbf67d321318a821dd2883960035fdb33
priyava <30829489+priyava@users.noreply.github.com>
2018-01-25 16:18:57 +0530

    Create strlength.java

---
+++ strlength.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class strlength
+{
+public static void main(String args[])
+{
+String str;
+System.out.println("\n Enter the string"+name[i]);
+System.out.println("\n The length of the string is:"+getLengthOfStringWithCharArray(str));
+}
+ppublic static int getLengthOfStringWithCharArray(String str)
+{
+int length=0;
+char[] strCharArray=str.toCharArray();
+for(char c:strCharArray)
+{
+length++;
+}
+return lengh;
+}
+}
