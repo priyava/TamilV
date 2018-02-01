49702860f81303dd25b0d790c3831972b5c61bcc
priyava <30829489+priyava@users.noreply.github.com>
2018-02-01 11:04:55 +0000

    Create length.c

---
+++ length.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[100];
+int l;
+printf("\n Enter the string");
+gets(str);
+l=strlen(str);
+printf("\n the total length is %d",l);
+}
+
