d4e35050dcbc350cf9e2e5dc40e01cfae8be92aa
priyava <30829489+priyava@users.noreply.github.com>
2018-02-09 08:57:21 +0000

    Update sumeven1.c

--- sumeven1.c
+++ sumeven1.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 void main()
 {
-int num1,num2,sum;
+int num1,num2,diff;
 printf("\n Enter the two numbers");
 scanf("%d %d",&num1,&num2);
-sum=num1+num2;
-if(sum%2==0)
+diff=num1-num2;
+if(diff%2==0)
 {
 printf("\n Even");
 }
 else
 {
-printf("\n Not even");
+printf("\n odd");
 }
 }
