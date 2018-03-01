ab7c02b154ec6d7b15ff98ca5e731d377e044584
priyava <30829489+priyava@users.noreply.github.com>
2018-03-01 16:43:08 +0530

    Create composite2.c

---
+++ composite2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int num,i;
+printf("\n enter the number");
+scanf("%d",&num);
+for(i=2;i<=num;i++)
+{
+if(num%i==0)
+{
+break;
+}
+if(num!=i)
+{
+printf("\n Composite");
+}
+}
+}
