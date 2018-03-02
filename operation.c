61a8e08da45a59dc5e7bfefb79ee5a6316a80786
priyava <30829489+priyava@users.noreply.github.com>
2018-03-02 15:43:37 +0530

    Create operation.c

---
+++ operation.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,op;
+printf("\na=");
+scanf("%d",&a);
+printf("\nb=");
+scanf("%d",&b);
+printf("\nc=");
+scanf("%d",&c);
+op=a*b%c;
+printf("\nANSWER=%d",op);
+}
